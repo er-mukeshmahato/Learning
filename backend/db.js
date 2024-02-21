278a7da1b572221a0383e59205537aec97425123
ermuk <singhmukesh.mm@gmail.com>
2024-02-22 01:11:34 +0900

    Your commit message

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose=require("mongoose");
+
+const mongoURI="mongodb://localhost:27017"
+
+const connectToMongo=()=>{
+    mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
+}
+module.exports=connectToMongo;
---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const connectToMongo = require("./db");
+
+const express = require("express");
+connectToMongo();
+const app = express();
+const port = 3000;
+app.use(express.json())
+//Available routes
+app.use("/api/auth", require("./routes/auth"));
+app.use('/api/note',require('./routes/note'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
